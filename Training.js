0f07b22af65ff7bee67297d8ad019d6074b7284a
ahmedheshammec <ahmed.hesham8728@gmail.com>
2024-02-20 23:59:44 +0200

    Finished JS OOP Course

--- Training.js
+++ Training.js
@@ -1,14 +1,24 @@
-const user = {
-  name: "Osama",
-  country: "Egypt",
-  age: 37,
-};
+class User {
+  constructor(fName, lName, age, email) {
+    this.name = {
+      first: fName,
+      last: lName,
+    };
+    this.age = age;
+    this.email = email;
+  }
+  sayHello = function () {
+    return `Say Hello`;
+  };
+  sayHi() {
+    return `Say Hi`;
+  }
+}; 
 
-let finalData = "";
-
-for (let info in user) {
-  finalData += `The ${info} Is => ${user[info]}\n`;
-};
-
-console.log(finalData);
-
+let user1 = new User("Osama", "Elzero", 37, "o@nn.sa");
+console.log(user1.name.first); // Output: Osama
+console.log(user1.name.last); // Output: Elzero
+console.log(user1.age); // Output: 37
+console.log(user1.email); // Output: o@nn.sa
+console.log(user1.sayHello()); // Output: Say Hello
+console.log(user1.sayHi()); // Output: Say Hi
